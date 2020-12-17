0286e8d0491f8809325b222a0bdfedfbabcd7128
Ateyyah-Buthor <ateyyah@buthor.ps>
2020-12-17 22:12:04 +0200

    Final Challenge Commit

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+const defaultPrimaryColor = document.documentElement.style
+    .getPropertyValue('--primary-color');
+const defaultSecondaryColor = document.documentElement.style
+    .getPropertyValue('--secondary-color');
+
+const primaryColorPicker = document.querySelector('#primary-picker');
+const secondaryColorPicker = document.querySelector('#secondary-picker');
+const resetButton = document.querySelector('.reset-button');
+
+primaryColorPicker.addEventListener('input', OnPrimaryColorChanged);
+secondaryColorPicker.addEventListener('input', OnSecondaryColorChanged);
+resetButton.addEventListener('click', colorReset);
+
 function OnPrimaryColorChanged() {
     let newColor = document.getElementById("primary-picker").value;
     document.documentElement.style
@@ -8,4 +21,11 @@ function OnSecondaryColorChanged() {
     let newColor = document.getElementById("secondary-picker").value;
     document.documentElement.style
         .setProperty('--secondary-color', newColor);
+}
+
+function colorReset() {
+    document.documentElement.style
+        .setProperty('--primary-color', defaultPrimaryColor);
+    document.documentElement.style
+        .setProperty('--secondary-color', defaultSecondaryColor);
 }
